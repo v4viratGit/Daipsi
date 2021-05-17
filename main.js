60dbc77cb053abb3223979756c427750ede15313
Virat Kamble <mrviratkamble98@gmail.com>
2021-05-17 14:38:28 +0530

    Virat-Added all front end files

---
+++ main.js
@@ -0,0 +1,550 @@
+// Main Slider Start
+
+$('.main-slider-img').slick({
+  centerMode: true,
+  centerPadding: '10px',
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '180px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '110px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '140px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 810,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 590,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 490,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '15px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+// Main Slider End
+
+// Most Popular Courses Slider Start
+
+$('.most-popular-courses').slick({
+  centerMode: true,
+  centerPadding: '260px',
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '70px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '135px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '140px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '15px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+// Most Popular Courses Slider End
+
+// Most Popular Videos Slider Start
+
+$('.most-popular-videos').slick({
+  centerMode: true,
+  centerPadding: '260px',
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '135px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '140px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '15px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+
+// Most Popular Videos Slider End
+
+//Commonly Asked Doubts Start
+
+$('.commonly-asked-doubts').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 4500,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '110px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '230px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '800px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 490,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+//Commonly Asked Doubts End
+
+//Recently Joined Students Start
+
+$('.recently-joined-students').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 4500,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '130px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '90px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '65px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+
+//Recently Joined Students End
+
+//Our Teachers Start
+
+$('.our-teachers').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 4800,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '130px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '90px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '65px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+
+//Our Teachers End
+
+//Our Ratings Start
+
+$('.our-ratings').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '130px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '45px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '15px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+
+//Our Ratings End
+
+//Top Running Courses Start
+
+$('.top-running-courses').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 4800,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '90px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '83px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+//Top Running Courses End
+
+// Partner's Slider Start
+
+$(document).ready(function () {
+$ ('.partners-img').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
+  });
+  });
+
+  // Partner's Slider End
+              
---
+++ search.php
@@ -0,0 +1,109 @@
+<?php
+    require 'dbconnection.php';
+    if(isset($_SESSION['email']))
+    {
+        $email=$_SESSION['email'];
+        $query = "SELECT name FROM users WHERE email='$email'";
+        $result = mysqli_query($con, $query) or die(mysqli_error($con));
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" 
+    crossorigin="anonymous">
+
+    <!-- styles.css -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Home</title>
+</head>
+<body>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light border-bottom border-primary mb-0" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="index.php">Diapsi</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <?php                 
+                    if (isset($_SESSION['email'])) 
+                    {                     
+                ?> 
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                    Welcome,
+                    <?php 
+                        while ($row = $result->fetch_assoc()) {
+                            echo $row['name']."!";
+                        }
+                    ?>
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="my-courses.php">My Courses <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="logout.php">Log out <span class="sr-only">(current)</span></a>
+                </li>   
+                <?php
+                    }else
+                    {
+                ?>  
+                <li class="nav-item active">
+                    <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="signup.php">Sign Up <span class="sr-only">(current)</span></a>
+                </li>
+                <?php
+                    }
+                ?>
+            </ul>
+        </div>
+    </nav>
+   <?php
+        $search=$_GET["search"];
+        $course=$_GET["course"];
+        $sql="SELECT * FROM $course WHERE MATCH(question) AGAINST ('%" . $search . "%')";
+        $run=mysqli_query($con,$sql) or die(mysqli_error($con));
+        $foundNum=mysqli_num_rows($run);
+        if($foundNum==0){
+            $table=$course."_queries";
+            echo "Sorry! we don't have the answer to your question, We'll try to post the answer within 24 hours.";
+            $insertQuery="insert into $table (question) VALUE ('$search, $course')";
+            $run=mysqli_query($con,$insertQuery) or die(mysqli_error($con));
+        } else
+            {
+            echo "<h1>$foundNum results found for your query $search</h1>";
+            $getQuery=mysqli_query($con,$sql);
+            while($runRows=mysqli_fetch_array($getQuery)){
+                echo $runRows['question'];   
+            }
+
+        }
+   ?>
+
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
+    crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" 
+    crossorigin="anonymous"></script>
+
+</body>
+</html>
